01fbd9a2e274af0209e72324c71617ac668279be
Sojeong1111 <banahs7029kq@naver.com>
2026-02-11 03:30:14 +0900

    main 수정실패

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* section -hero 스와이퍼 사용 */
+new Swiper('.hero-slider', {
+	loop: true,
+	speed: 800,
+	autoplay: {
+		delay: 7000,
+		disableOnInteraction: false,
+	},
+});
+
+/*----------------------------------------------------------------- */
