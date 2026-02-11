3f1b6ae117d9e5c0548085540a016d44a3e957e5
Sojeong1111 <banahs7029kq@naver.com>
2026-02-11 23:58:20 +0900

    메인 색 수정 전

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 /* section -hero 스와이퍼 사용 */
-new Swiper('.hero-slider', {
-	loop: true,
-	speed: 800,
-	autoplay: {
-		delay: 7000,
-		disableOnInteraction: false,
-	},
-});
 
 /*----------------------------------------------------------------- */
